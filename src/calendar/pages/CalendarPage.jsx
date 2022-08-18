3315c9f8571653ec0563e4950e010beebb70394c
Luis Leal <luisrrleal@gmail.com>
2022-08-17 19:55:48 -0500

    React big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
-import { Navbar } from "../components"
+import { Navbar } from "../components";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+
+import { format, parse, startOfWeek, getDay, addHours } from "date-fns";
+
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Luis Birthday',
+  notes: 'Have to buy the cake',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Leal'
+  }
+
+}]
 
 export const CalendarPage = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
-  )
-}
+  );
+};
